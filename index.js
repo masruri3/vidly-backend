ee9f618e45bfdac7bc08426edfd9141e5bb22cf0
masrurimz <masruri03@gmail.com>
2020-11-27 22:16:36 +0700

    added user login

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require("mongoose");
 
+const auth = require("./routes/auth");
 const customer = require("./routes/customers");
 const genres = require("./routes/genres");
 const movies = require("./routes/movies");
@@ -24,6 +25,7 @@ mongoose
 
 app.use(express.json());
 
+app.use("/api/auth", auth);
 app.use("/api/customers", customer);
 app.use("/api/genres", genres);
 app.use("/api/movies", movies);
