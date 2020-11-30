b184fa4f0d0ac5da7b3f310372f61a4e40b21889
masrurimz <masruri03@gmail.com>
2020-11-30 22:29:06 +0700

    Added get user info routes

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,16 @@ const bcrypt = require("bcrypt");
 const express = require("express");
 const router = express.Router();
 
+const auth = require("../middleware/auth");
 const { User, validate } = require("../models/user");
+
+/**
+ * @access Pirvate logged In user
+ */
+router.get("/me", auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select("-password");
+  res.send(user);
+});
 
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
