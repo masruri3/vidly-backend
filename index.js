d42caad8f4631f53e0321ff3dc4a2efda3378099
masrurimz <masruri03@gmail.com>
2020-12-02 18:27:49 +0700

    refactored db code

--- index.js
+++ index.js
@@ -3,19 +3,13 @@ const config = require("config");
 const express = require("express");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
-const mongoose = require("mongoose");
 const winston = require("winston");
 require("winston-mongodb");
 
-const auth = require("./routes/auth");
-const customer = require("./routes/customers");
-const error = require("./middleware/error");
-const genres = require("./routes/genres");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
-
 const app = express();
+
+require("./startup/routes")(app);
+require("./startup/db")();
 
 process.on("uncaughtException", (ex) => {
   winston.error(ex.message, ex);
@@ -48,27 +42,6 @@ if (!config.has("mongoURI")) {
   console.error("FATAL ERROR: jwtPrivateKey is not defined.");
   process.exit(1);
 }
-
-mongoose
-  .connect(config.get("mongoURI"), {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  })
-  .then(() => console.log("Connected to MongoDB..."))
-  .catch((err) => console.error("Could not connect to MongoDB..."));
-
-app.use(express.json());
-
-app.use("/api/auth", auth);
-app.use("/api/customers", customer);
-app.use("/api/genres", genres);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-
-app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
