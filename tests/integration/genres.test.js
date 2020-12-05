0bd0e5f82959f16118f8564ed7655f7096b6529b
masrurimz <masruri03@gmail.com>
2020-12-05 13:14:48 +0700

    added GET genre/id test

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,4 +1,6 @@
 const request = require("supertest");
+const { Genre } = require("../../models/genre");
+const monggoose = require("mongoose");
 
 let server;
 
@@ -6,12 +8,37 @@ describe("/api/genres", () => {
   beforeEach(() => {
     server = require("../../index");
   });
-  afterEach(() => server.close());
+  afterEach(async () => {
+    server.close();
+    await Genre.remove({});
+  });
 
   describe("GET /", () => {
     it("should return all genres", async () => {
+      await Genre.collection.insertMany([
+        { name: "genre1" },
+        { name: "genre2" },
+      ]);
+
       const res = await request(server).get("/api/genres");
       expect(res.status).toBe(200);
+      expect(res.body.length).toBe(2);
+      expect(res.body.some((g) => g.name === "genre1")).toBeTruthy();
+      expect(res.body.some((g) => g.name === "genre2")).toBeTruthy();
+    });
+  });
+
+  describe("GET /:id", () => {
+    it("should return genre if valid id is passed", async () => {
+      const genre = new Genre({
+        name: "genre1",
+      });
+      await genre.save();
+
+      const res = await request(server).get("/api/genres/" + genre._id);
+
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty("name", genre.name);
     });
   });
 });
