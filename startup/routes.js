5bde4658b0b38545817caa5f7d20f1fd7f34e1c9
masrurimz <masruri03@gmail.com>
2020-12-06 10:47:35 +0700

    added return routes

--- startup/routes.js
+++ startup/routes.js
@@ -6,6 +6,7 @@ const error = require("../middleware/error");
 const genres = require("../routes/genres");
 const movies = require("../routes/movies");
 const rentals = require("../routes/rentals");
+const returns = require("../routes/returns");
 const users = require("../routes/users");
 
 module.exports = function (app) {
@@ -16,6 +17,7 @@ module.exports = function (app) {
   app.use("/api/genres", genres);
   app.use("/api/movies", movies);
   app.use("/api/rentals", rentals);
+  app.use("/api/returns", returns);
   app.use("/api/users", users);
 
   app.use(error);
